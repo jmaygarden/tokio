3d4d85bf95899bd245f6be7695a313c6a20ac166
Jon Magnuson <jon.magnuson@gmail.com>
2020-11-17 16:12:11 -0600

    wip! cherry-picked everything over(ish)
    and started fixing `cargo check` stuff before realizing it would be
    better to separate that from the initial cherry.

--- tokio/src/runtime/mod.rs
+++ tokio/src/runtime/mod.rs
@@ -296,6 +296,10 @@ cfg_rt! {
     /// After thread starts / before thread stops
     type Callback = std::sync::Arc<dyn Fn() + Send + Sync>;
 
+    /// Takes the `Duration` for the underlying `Park` and uses it, giving back
+    /// some modified version of the duration for use.
+    pub type ParkShim = std::sync::Arc<std::sync::Mutex<Box<dyn FnMut(Option<Duration>) -> Option<Duration> + Send + Sync>>>;
+
     impl Runtime {
         /// Create a new runtime instance with default configuration values.
         ///
@@ -556,6 +560,18 @@ cfg_rt! {
         /// ```
         pub fn shutdown_background(self) {
             self.shutdown_timeout(Duration::from_nanos(0))
+        }
+
+        /// Craps out a RawFd for *nix systems, if one exists.
+        #[cfg(unix)]
+        pub fn driver_fd(&self) -> Option<std::os::unix::io::RawFd> {
+            use std::os::unix::io::AsRawFd;
+            match self.kind {
+                Kind::Basic(ref basic_scheduler) => {
+                    Some(basic_scheduler.as_raw_fd())
+                }
+                _ => None,
+            }
         }
     }
 }
